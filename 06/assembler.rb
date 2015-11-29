641c4b95d1c0fbf8f3b470a542b88dc300c4224b
Eileen Guo <eileen.m.guo@gmail.com>
2015-11-28 23:05:09 -0600

    created the hack filename based on the asm file

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -9,6 +9,13 @@ def is_readable?(path)
 	File.readable?(path)
 end
 
+def hack_filename(asm_filename)
+	asm_basename = File.basename(asm_filename, ".asm")
+	path = File.split(asm_filename)[0]
+	"#{path}/#{asm_basename}.hack"
+end
+
+
 unless args_valid?
 	abort("Usage: ./assembler.rb Prog.asm")
 end
@@ -18,5 +25,9 @@ unless is_readable?(asm_filename)
 	abort("#{asm_filename} not found or is unreadable")
 end
 
-asm_file = File.open(asm_filename)
-puts asm_file.read
+File.open(asm_filename) do |asm_file|
+	File.open(hack_filename(asm_filename), 'w') do |hack_file|
+		assembler = Assembler.new(asm_file, hack_file)
+		assembler.assemble
+	end
+end 
