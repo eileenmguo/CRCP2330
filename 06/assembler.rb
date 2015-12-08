992fc2e9df34d63cfd51687d1530a7998400c428
Eileen Guo <eileen.m.guo@gmail.com>
2015-12-07 20:19:45 -0600

    Successfully with Rect.asm and RectL.asm

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -30,6 +30,8 @@ class Assembler
 					line.gsub! line[1..-1], labels[line[1..-1]].to_s
 				elsif not line.match(/@\d+/)
 					labels[line[1..-1]] = 16 + numVariables
+					line.gsub! line[1..-1], labels[line[1..-1]].to_s
+					numVariables += 1
 				end 
 			end
 		end
